8d7e5f138a685d751acb40a2c45f2e1fd43dc1ad
Andrii Drobiazko <c2q9450@gmail.com>
2018-10-15 21:11:03 +0300

    Section 12 finished

---
+++ tech_stack/src/components/common/Header.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+
+const Header = (props) => {
+    const { textStyle, viewStyle } = styles;
+    return (
+        <View style={viewStyle}>
+            <Text style={textStyle} >{props.headerText}</Text>
+        </View>
+
+    );
+};
+
+const styles = {
+    viewStyle: {
+        backgroundColor: '#FEFeFe',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 60,
+        elevation: 8,
+        paddingTop: 15,
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2
+
+        },
+        shadowOpacity: 0.2,
+        position: 'relative'
+    },
+    textStyle: {
+        fontSize: 20,
+        color: '#000',
+    }
+
+};
+
+export { Header };
