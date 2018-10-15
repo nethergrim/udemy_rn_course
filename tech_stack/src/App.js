ab0c2306cb7c9fb7c006b19ecbd656b0a5960f00
Andrii Drobiazko <c2q9450@gmail.com>
2018-10-15 20:20:16 +0300

    react-redux deps

--- tech_stack/src/App.js
+++ tech_stack/src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { View } from "react-native";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
 const App = () => {
-  return <View />;
+  return (
+    <Provider store={createStore}>
+      <View />
+    </Provider>
+  );
 };
-
 
 export default App;
