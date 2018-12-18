66b52a4f2052907cd5c72228557cb20598a43dd3
Andrii Drobiazko <c2q9450@gmail.com>
2018-12-18 16:47:26 +0200

    Added router

--- manager/src/reducers/AuthReducer.js
+++ manager/src/reducers/AuthReducer.js
@@ -17,7 +17,7 @@ export default (state = INITIAL_STATE, action) => {
         case PASSWORD_CHANGED:
             return { ...state, password: action.payload };
         case LOGIN_USER_SUCCESS:
-            return { ...state, user: action.payload, loading: false, error: '' };
+            return { ...state, ...INITIAL_STATE, user: action.payload };
         case LOGIN_STARTED:
             return { ...state, loading: true, error: '', user: undefined }
         case LOGIN_ERROR:
