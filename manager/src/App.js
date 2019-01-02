db857d40d36c5b8eb8bb513bb122234ae79fd235
Andrii Drobiazko <c2q9450@gmail.com>
2019-01-02 16:40:44 +0200

    Updated react-router-flux

--- manager/src/App.js
+++ manager/src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
   render() {
     return (
       <Provider store={createStore(reducers, {}, applyMiddleware(ReduxThunk))}>
-        <SafeAreaView>
+        <SafeAreaView style={{flex: 1}}>
           <Router />
         </SafeAreaView>
       </Provider>
